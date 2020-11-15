70539c9baf46f807482c45f72ccdf3558df35b82
Jitender Singh <jitender.singh@salesforce.com>
2020-11-15 22:20:21 +0530

    file for analysing algo performance

---
+++ analysis.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+from os import system
+
+args = ['python3', './bloxorz.py']
+
+# BFS, DFS
+for perm in permutations(['L', 'U', 'R', 'D']):
+    for algorithm in ['bfs', 'dfs']:
+        opts = ['-o', ''.join(perm), '-s', algorithm]
+        system(' '.join(args + opts))
+
+        print("\n\nalgorithm: {}, order: {}".format(algorithm, ''.join(perm)))
+        input("press ENTER to continue - ")
+
+# A*
+for perm in permutations(['L', 'U', 'R', 'D']):
+    for cost_heuristic in ['euclidean', 'manhattan']:
+        opts = ['-o', ''.join(perm), '-s', 'a-star', '-c', cost_heuristic]
+        system(' '.join(args + opts))
+
+        print("\n\nalgorithm: A*, order: {}, heuristic: {}".format(''.join(perm), cost_heuristic))
+        input("press ENTER to continue - ")
+
